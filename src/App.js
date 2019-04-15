1407b50e2324feba742e4615f2ab0017bd975933
Joe Morgan <jsmapr1@gmail.com>
2019-04-15 16:35:42 -0500

    basic list stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Builder from './components/Builder/Builder';
 
 class App extends Component {
   render() {
@@ -11,14 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Builder />
         </header>
       </div>
     );
